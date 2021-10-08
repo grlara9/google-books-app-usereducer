016ccdaebcff9a4abbbc7e4e23f11f457e5d5e25
Guillermo <grlara9@gmail.com>
2021-10-07 23:34:35 -0500

    create usefetch compoent will use to fetch data from api

---
+++ src/components/useFetch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import axios from 'axios'
+
+
+const BASE_URL = "https://cors-anywhere.herokuapp.com/https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=5";
+const useFetch = (params, page) => {
+return <div></div>
+}

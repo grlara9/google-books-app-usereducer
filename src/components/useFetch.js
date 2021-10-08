d05d9b9be7474a766e3a7302aa86a515766f8c31
Guillermo <grlara9@gmail.com>
2021-10-07 23:47:28 -0500

    defines actions nd create reducer function

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -3,6 +3,21 @@ import axios from 'axios'
 
 
 const BASE_URL = "https://cors-anywhere.herokuapp.com/https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=5";
+
+const ACTIONS = {
+    MAKE_REQUEST: "make-request",
+    GET_DATA: "get-data",
+    ERROR: "error",
+    NEXT_PAGE: "next-page",
+  };
+
+function reducer(state, action){
+    switch(action.type){
+        case ACTIONS.MAKE.REQUEST:
+            return{loading: true, games:[]}
+    }
+}
+
 const useFetch = (params, page) => {
 return <div></div>
 }

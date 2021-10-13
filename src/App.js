21b0e2015a8c9ef41a4418c0ae718d2e400c2419
Guillermo <grlara9@gmail.com>
2021-10-12 23:31:25 -0500

    handler function for paramenter change is implemented

--- src/App.js
+++ src/App.js
@@ -9,10 +9,20 @@ function App() {
   const [page, setPage] = useState(1)
   const {games, loading, error} = useFetch(params, page)
 
-  console.log(games, loading)
+  console.log(games, loading, params)
 
+
+  const handleParamsChange = (e)=>{
+    const param = e.target.name
+    const value= e.target.value
+    setPage(0);
+    setParams((prevParams) =>{
+      return { ...prevParams, [param]: value}
+    })
+  }
   return (
     <div className="App">
+      <Form params={params} onParamChange={handleParamsChange} />
      {loading && <h1>Loading...</h1>}
      {error && <h1>{error.message}</h1>}
     </div>

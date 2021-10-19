1fad125a98d3cee74266d6992a9f0cecf68ea4f6
Guillermo <grlara9@gmail.com>
2021-10-19 10:01:52 -0500

    install react mark down into project

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,57 +1,42 @@
 import React, { useState } from "react";
-import { Card, Button, Collapse } from "react-bootstrap";
+import {Card , Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
+const Books = ({ book }) => {
 
-export default function Game({ game }) {
-  const [open, setOpen] = useState(false);
-
+  const [open, setOpen]= useState(false)
+  
   return (
-    <Card className="mb-3">
-      <Card.Body>
-        <div className="d-flex justify-content-between">
-          <div>
-            <Card.Title>
-              {game.title} -{" "}
-              <span
-                className="text-muted font-weight-light"
-                style={{ textDecoration: "line-through" }}
-              >
-                ${game.normalPrice}
-              </span>
-              <span>{" - "}</span>
-              <span className="font-weight-light">${game.salePrice}</span>
-            </Card.Title>
-            <Card.Subtitle className="text-muted mb-2">
-              Release Date:{" "}
-              {new Date(game.releaseDate * 1000).toLocaleDateString()}
-            </Card.Subtitle>
+      <Card className="mb-3">
+        <Card.Body>
+         <div className="d-flex justify-content-between">
+            <div>
+                <Card.Title>
+                  <h3>{book.volumeInfo.title} </h3>
+                </Card.Title>
+                <Card.Subtitle className="text-muted mb-2">
+                  <span className="text-muted font-weight-light mr-2">by {book.volumeInfo.authors }</span> | {new Date(book.volumeInfo.publishedDate).toLocaleDateString()}
+                </Card.Subtitle>
+                <Badge><span color="black">{book.volumeInfo.categories}</span></Badge>
+                <div>
+                  <ReactMarkdown className="mr-2">{book.volumeInfo.previewLink}</ReactMarkdown>
+                </div>
+            </div>
+            <img className="d-none d-md-block" height="150"src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}/>
           </div>
-          <img
-            className="d-none d-md-block"
-            height="50"
-            alt={game.title}
-            src={game.thumb}
-          />
+      <Card.Text>
+        <Button onClick={() => setOpen(prevOpen => !prevOpen)} variant="primary">
+            {open ? 'Hide Details' : 'View Details'}
+        </Button>
+      </Card.Text>
+      <Collapse in={open}>
+        <div className="mt-4">
+          <ReactMarkdown>{book.volumeInfo.description}</ReactMarkdown>
         </div>
-        <Card.Text>
-          <Button
-            onClick={() => setOpen((prevOpen) => !prevOpen)}
-            variant="primary"
-          >
-            {open ? "Hide Details" : "View Details"}
-          </Button>
-        </Card.Text>
-        <Collapse in={open}>
-          <div className="mt-4">
-            <div>Metacritic Score: {game.metacriticScore}</div>
-            <Button
-              variant="success"
-              href={`https://store.steampowered.com/app/${game.steamAppID}/`}
-            >
-              Check it out
-            </Button>
-          </div>
-        </Collapse>
-      </Card.Body>
-    </Card>
-  );
+      </Collapse>
+    </Card.Body>
+  </Card>
+    
+     
+  )
 }
+export default Books;

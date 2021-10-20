ba45f105ebeea0ceeafc00fc546f3534102a92f3
Guillermo <grlara9@gmail.com>
2021-10-20 08:57:32 -0500

    implementing pagination component will take some props implementing back button and next page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,7 +5,21 @@ import { Pagination } from 'react-bootstrap'
 export default function GamesPagination({ page, setPage, hasNextPage }) {
 return(
     <Pagination>
-         <Pagination.Item onClick={() => setPage(0)}>1</Pagination.Item>
+         {page !== 0 && <Pagination.Prev onClick={() => adjustPage(-1)} />}
+      {page !== 0 && (
+        <Pagination.Item onClick={() => setPage(0)}>1</Pagination.Item>
+      )}
+      {page > 1 && <Pagination.Ellipsis />}
+      {page > 1 && (
+        <Pagination.Item onClick={() => adjustPage(-1)}>{page}</Pagination.Item>
+      )}
+      <Pagination.Item active>{page + 1}</Pagination.Item>
+      {hasNextPage && (
+        <Pagination.Item onClick={() => adjustPage(1)}>
+          {page + 2}
+        </Pagination.Item>
+      )}
+      {hasNextPage && <Pagination.Next onClick={() => adjustPage(1)} />}
     </Pagination>
 )
 }
